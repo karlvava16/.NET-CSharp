a38dd8ef3ad5499ca9ccdf1e8813dacda257dc2e
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-08 14:49:17 +0200

    lab_08.02.2023

---
+++ lab_08.02.2023/lab_08.02.2023/BankCard.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankCard
+    {
+        private string bankName;
+        private string cardNumber;
+        private string cardCVV;
+        DateTime cardDedline;
+
+        public string BankName { get { return bankName; } set { bankName = value; } }
+
+        public string CardNumber
+        {
+            get { return cardNumber; }
+            set
+            {
+                string temp = value.ToString().Replace(" ", "");
+                try
+                {
+                    if (temp.Length != 16)
+                    {
+                        throw new Exception("Unaccessable card number length");
+                    }
+
+                    foreach (var el in temp)
+                    {
+                        if (!(el >= '0' && el <= '9'))
+                        {
+                            throw new Exception("Unaccessable symbol in card number");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                cardNumber = temp;
+            }
+        }
+
+        public string CardCVV 
+        { 
+            get { return cardCVV; }
+            set 
+            {
+                string temp = value.ToString().Replace(" ", "");
+                if (temp.Length != 3)
+                {
+                    throw new Exception("Unaccessable CVV length");
+                }
+
+                foreach (var el in temp)
+                {
+                    if (!(el >= '0' && el <= '9'))
+                    {
+                        throw new Exception("Unaccessable symbol in CVV");
+                    }
+                }
+
+                cardCVV = temp;
+            }
+        }
+
+        public DateTime CardDedline { get { return cardDedline; } set { cardDedline = value; } }
+
+
+        public BankCard() { }
+
+        public BankCard(string bankName)
+        {
+            BankName = bankName;
+        }
+
+        public BankCard(string bankName, string card_number) : this(bankName)
+        {
+            CardNumber = card_number;
+        }
+
+        public BankCard(string bankName, string cardNumber, string cardCVV) : this(bankName, cardNumber)
+        {
+            CardCVV = cardCVV;
+        }
+
+        public BankCard(string bankName, string cardNumber, string cardCVV, string cardDedline) : this(bankName, cardNumber, cardCVV)
+        { 
+           CardDedline = DateTime.ParseExact(cardDedline, "MM/yy", CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"Bank : {BankName}\nCard number : {cardNumber}\nCVV : {cardCVV}\nCard dedline : {CardDedline.ToString("MM/yy")}\n";
+        }
+    }
+}
---
+++ lab_08.02.2023/lab_08.02.2023/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bank;
+
+namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankCard card = new BankCard("MonoBank", "1234 5678 9111 1121", "512", "10/25");
+            card.CardCVV = "BB1";
+            Console.WriteLine(card);
+        }
+    }
+}
