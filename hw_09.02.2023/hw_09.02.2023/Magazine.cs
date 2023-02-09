228e896aedf999ab8af2d04a25e370167292b32e
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-09 15:13:40 +0200

    hw_09.02.2023

---
+++ hw_09.02.2023/hw_09.02.2023/Magazine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_06._02._2023
+{
+    public class Magazine
+    {
+        string name;
+        string year;
+        string description;
+        string phone;
+        string email;
+
+        public uint Staff { get; set; }
+        public string Name { get { return name; } set { name = value; } }
+        public string Year { get { return year; } set { year = value; } }
+        public string Phone { get { return phone; } set { phone = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public Magazine() { }
+
+        public Magazine(string name, string year, string description, string phone, string email, uint staff)
+        { 
+            Name = name;
+            Year = year;
+            Phone = phone;
+            Email = email;
+            Description = description;
+            Staff = staff;
+        }
+
+        public static Magazine operator + (Magazine m, uint staff)
+        {
+            Magazine temp = m;
+            temp.Staff = temp.Staff + staff;
+            return temp;
+        }
+
+        public static Magazine operator -(Magazine m, uint staff)
+        {
+            Magazine temp = m;
+            temp.Staff = temp.Staff - staff;
+            return temp;
+        }
+
+        public static Magazine operator +(uint staff, Magazine m)
+        {
+            Magazine temp = m;
+            temp.Staff =  staff + temp.Staff;
+            return temp;
+        }
+        public static Magazine operator -( uint staff, Magazine m)
+        {
+            Magazine temp = m;
+            temp.Staff = staff - temp.Staff;
+            return temp;
+        }
+
+        public static bool operator ==(Magazine a, Magazine b)
+        {
+            return a.Staff == b.Staff;
+        }
+        public static bool operator !=(Magazine a, Magazine b)
+        {
+            return a.Staff != b.Staff;
+        }
+        public static bool operator >(Magazine a, Magazine b)
+        {
+            return a.Staff > b.Staff;
+        }
+        public static bool operator <(Magazine a, Magazine b)
+        {
+            return a.Staff < b.Staff;
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {Name}\nYear : {Year}\nPhone : {Phone}\nEmail : {Email}\nDescription : {Description}\nStaff : {Staff}";
+        }
+    }
+}
---
+++ hw_09.02.2023/hw_09.02.2023/Store.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Store
+    {
+        string name;
+        string address;
+        string description;
+        string phone;
+        string email;
+        public double Square { get; set; }
+        public string Name { get { return name; } set { name = value; } }
+        public string Address { get { return address; } set { address = value; } }
+        public string Description { get { return description; } set { description = value; } }
+        public string Phone { get { return phone; } set { phone = value; } }
+        public string Email { get { return email; } set { email = value; } }
+
+        public Store() { }
+
+        public Store (string name, string address, string description, string phone, string email, double square)
+        {
+            Name = name;
+            Address = address;
+            Description = description;
+            Phone = phone;
+            Email = email;
+            Square = square;
+        }
+
+
+        public static Store operator+(Store s, double sq)
+        {
+            Store temp = s;
+            temp.Square += sq;
+            return temp;
+        }
+
+        public static Store operator -(Store s, double sq)
+        {
+            Store temp = s;
+            temp.Square -= sq;
+            return temp;
+        }
+
+        public static Store operator +( double sq, Store s)
+        {
+            Store temp = s;
+            temp.Square = sq + temp.Square;
+            return temp;
+        }
+
+        public static Store operator -(double sq, Store s)
+        {
+            Store temp = s;
+            temp.Square = sq - temp.Square;
+            return temp;
+        }
+
+        public static bool operator ==(Store s1, Store s2)
+        {
+            return s1.Square == s2.Square;
+        }
+        public static bool operator !=(Store s1, Store s2)
+        {
+            return s1.Square != s2.Square;
+        }
+
+        public static bool operator <(Store s1, Store s2)
+        {
+            return s1.Square < s2.Square;
+        }
+        public static bool operator >(Store s1, Store s2)
+        {
+            return s1.Square > s2.Square;
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {Name}\nAddress : {Address}\nDescription : {Description}\nPhone : {Phone}\nEmail : {Email}\nSquare : {Square}";
+        }
+    }
+}
