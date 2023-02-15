cde3bf05917c0eaa83ce1cb7a8ae82140c37ae34
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-15 14:19:30 +0200

    hw_15.02.2023

---
+++ hw_15.02.2023/hw_15.02.2023/Book.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Book : IComparable, ICloneable
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+        public uint PageAmount { get; set; }
+
+
+        public Book() { }
+
+        public Book(string name, string author, string description, uint pageAmount)
+        {
+            Name = name;
+            Description = description;
+            Author = author;
+            PageAmount = pageAmount;
+        }
+
+
+        public static bool operator ==(Book a, Book b)
+        {
+            if (a.Name != b.Name)
+                return false;
+            if (a.Author != b.Author)
+                return false;
+            if (a.PageAmount != b.PageAmount)
+                return false;
+            if (a.Description != b.Description)
+                return false;
+            return true;
+        }
+
+        public static bool operator !=(Book a, Book b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator >(Book a, Book b)
+        {
+            return a.PageAmount > b.PageAmount;
+        }
+
+        public static bool operator <(Book a, Book b)
+        {
+            return a.PageAmount < b.PageAmount;
+        }
+
+        public static bool operator >=(Book a, Book b)
+        {
+            return a.PageAmount >= b.PageAmount;
+        }
+
+        public static bool operator <=(Book a, Book b)
+        {
+            return a.PageAmount <= b.PageAmount;
+        }
+
+        public override string ToString()
+        {
+            return $"Book name : {Name}\nAuthor : {Author}\nPage Amount : {PageAmount}\nDescription :\n{Description}\n";
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Name.CompareTo((obj as Book).Name);
+            throw new NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            return new Book(Name, Author, Description, PageAmount);
+        }
+
+        public class SortByName : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).Name.CompareTo((obj2 as Book).Name);
+                throw new NotImplementedException();
+            }
+        }
+
+        public class SortByAuthor : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).Author.CompareTo((obj2 as Book).Author);
+                throw new NotImplementedException();
+            }
+        }
+
+        public class SortByPage : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).PageAmount.CompareTo((obj2 as Book).PageAmount);
+                throw new NotImplementedException();
+            }
+        }
+
+        public class SortByDescription : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Book && obj2 is Book)
+                    return (obj1 as Book).Description.CompareTo((obj2 as Book).Description);
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ hw_15.02.2023/hw_15.02.2023/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library bs = new Library(new Book("book1", "me3", "cool", 200), new Book("book2", "me1", "not so cool", 5000));
+            bs.Push(new Book("book3", "m1", "better ^)", 300), 0);
+
+
+            foreach (Book book in bs) 
+            {
+            Console.WriteLine(book);
+            Console.WriteLine("-------------------------------");
+            }
+
+            Array.Sort(bs.Books, new Book.SortByPage());
+
+
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+
+
+            foreach (Book book in bs)
+            {
+                Console.WriteLine(book);
+                Console.WriteLine("-------------------------------");
+            }
+        }
+    }
+}
