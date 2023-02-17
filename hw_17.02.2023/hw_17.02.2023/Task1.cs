0167141e8c7f1556d492a1d062052f02d46eea7e
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-17 20:19:37 +0200

    hw_17.02.2023

---
+++ hw_17.02.2023/hw_17.02.2023/Task1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    abstract class Task1
+    {
+        public static bool Even(int value)
+        {
+            if(value % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Odd(int value)
+        {
+            if (value % 2 != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Prime(int value)
+        {
+            if (value <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < value; i++)
+            {
+                if(value % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Fib(int value)
+        {
+            if(value < 0)
+                throw new ArgumentOutOfRangeException();
+            if(value == 0)
+                return true;
+
+            for (int i = 0, j = 1; i <= value;)
+            {
+                (j, i) = (i, j);
+                j += i;
+                if (j == value)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ hw_17.02.2023/hw_17.02.2023/Task2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework
+{
+    delegate void Task2D();
+    abstract class Task2
+    {
+
+        public static void ShowTime()
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.TimeOfDay);
+        }
+        public static void ShowDate()
+        {
+
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.Date);
+
+        }
+
+        public static void ShowWeekDay()
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+        }
+
+        public static void ShowSTriangle()
+        {
+            Console.WriteLine("S triangle");
+        }
+        public static void ShowSRecktangle()
+        {
+            Console.WriteLine("S Recktangle");
+        }
+    }
+}
