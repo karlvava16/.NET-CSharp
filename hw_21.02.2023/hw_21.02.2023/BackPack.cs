c6d86bcbc047a54a9f462c727ad3c903e2d7c95c
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-22 13:53:36 +0200

    hw_21.02.2023

---
+++ hw_21.02.2023/hw_21.02.2023/BackPack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+
+    delegate void MyDelegate(Items a);
+
+    class Backpack
+    {
+        public event MyDelegate addItem;
+
+
+        string Color { get; set; }
+        string Firm { get; set; }
+        string Fabric { get; set; }
+        double Mass { get; set; }
+        public double V { get; set; }
+
+        List<Items> Objects = new List<Items>(16);
+
+        public Backpack() { }
+        public Backpack(string color, string firm, string fabric, double mass, double v)
+        {
+            Color = color;
+            Firm = firm;
+            Fabric = fabric;
+            Mass = mass;
+            V = v;
+
+            addItem = new MyDelegate((Items a) =>
+            {
+                if (V - a.v >= 0)
+                {
+                    Objects.Add(a);
+                    V += a.v;
+                }
+                else
+                {
+                    throw new Exception("Error");
+                }
+            });
+
+
+        }
+
+        public void AddItem(Items A)
+        {
+            addItem?.Invoke(A);
+        }
+
+
+    }
+
+    class Items
+    {
+
+        string name;
+        public double v { get; set; }
+
+        public Items(string name, double v)
+        {
+            this.name = name;
+            this.v = v;
+        }
+    }
+    
+}
---
+++ hw_21.02.2023/hw_21.02.2023/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework
+{
+    class Program
+    {
+        delegate int[] Rainbow(int x);
+        static void Main(string[] args)
+        {
+
+
+
+            Rainbow rgb = delegate (int x)
+            {
+                int[] RGB = { 0, 0, 0 };
+
+                switch (x)
+                {
+                    case 1: RGB[0] = 255; break;
+                    case 2: RGB[0] = 252; RGB[1] = 102; break;
+                    case 3: RGB[0] = 255; RGB[1] = 255; break;
+                    case 4: RGB[0] = 0; RGB[1] = 255; break;
+                    case 5: RGB[1] = 0; RGB[1] = 128; RGB[2] = 255; break;
+                    case 6: RGB[1] = 0; RGB[1] = 0; RGB[2] = 255; break;
+                    case 7: RGB[1] = 128; RGB[1] = 0; RGB[2] = 255; break;
+
+
+                    default:
+                        break;
+                }
+
+
+
+                return RGB;
+            };
+
+            int[] rgbb = rgb(1);
+            foreach (int item in rgbb)
+            {
+                Console.WriteLine(item);
+            }
+
+            //task2
+
+
+            Backpack backpack = new Backpack("color", "firm", "textile", 1, 8);
+
+            Items item1 = new Items("Item", 10);
+            Console.WriteLine(backpack.V);
+            backpack.AddItem(item1);
+            Console.WriteLine(backpack.V);
+
+
+            //task 3
+            int[] a = { -11, -2, 3, 4, 5, 6, 7, 8, 9, 10, 7 };
+            Mass kol = (int[] x) =>
+            {
+                int sum = 0;
+
+                foreach (int item in x)
+                {
+                    if (item % 7 == 0)
+                    {
+                        sum++;
+                    }
+                }
+                return sum;
+            };
+            Mass Plus = (int[] x) =>
+            {
+                int sum = 0;
+
+                foreach (int item in x)
+                {
+                    if (item > 0)
+                    {
+                        sum++;
+                    }
+                }
+                return sum;
+            };
+
+            Mass Otricatel = (int[] x) =>
+            {
+                int sum = 0;
+                bool unique = true;
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] < 0)
+                    {
+                        for (int z = 0; z < x.Length; z++)
+                        {
+                            if (i != z)
+                            {
+                                x[i] = x[z];
+                                unique = false;
+                            }
+                        }
+
+                    }
+                    if (unique)
+                    {
+                        sum++;
+                    }
+                }
+                return sum;
+            };
+            Console.WriteLine("Chisla kratni 7" + kol(a));
+            Console.WriteLine("Chisla Polozhitelni " + Plus(a));
+            Console.WriteLine("Chisla Otrizatel unique " + Otricatel(a));
+
+
+            Text cont = (string text, string word) =>
+            {
+                return text.Contains(word);
+            };
+
+
+        }
+
+        delegate int Mass(int[] x);
+        delegate bool Text(string text, string word);
+    }
+
+}
