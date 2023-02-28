a2cc34ee86dbec3729c860e361b2283e604d3917
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-28 11:42:34 +0200

    lab_28.02.2023

---
+++ lab_28.02.2023/lab_28.02.2023/EnglishRussianDict.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class EnglishRussianDict
+    {
+        SortedDictionary<string, string> englishRussianDict = new SortedDictionary<string, string>();
+
+        void AddWord()
+        {
+            Console.Clear();
+
+            string eng;
+            string rus;
+
+            Console.Write("Enter eng: ");
+            eng = Console.ReadLine();
+            if (englishRussianDict.ContainsKey(eng))
+            {
+                Console.WriteLine("Already exist");
+                return;
+            }
+            Console.Write("Enter rus: ");
+            rus = Console.ReadLine();
+            englishRussianDict.Add(eng, rus);
+            Console.ReadKey();
+        }
+
+        void DeleteWord()
+        {
+            Console.Clear();
+
+            bool choise;
+            string word;
+            Console.Write("Enter eng(true) or rus(false): ");
+            choise = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Enter word to remove: ");
+            word = Console.ReadLine().ToLower();
+
+            if (choise) //enghish
+            {
+                if (englishRussianDict.ContainsKey(word))
+                    englishRussianDict.Remove(word);
+                else
+                    Console.WriteLine(word + "doesn't exist");
+            }
+            else
+            {
+                if (englishRussianDict.ContainsValue(word))
+                {
+                    englishRussianDict.Remove(word);
+                }
+                else
+                    Console.WriteLine(word + "doesn't exist");
+            }
+            Console.ReadKey();
+
+        }
+
+        void FindE()
+        {
+            string word;
+            Console.Write("Enter english word: ");
+            word = Console.ReadLine().ToLower();
+            if (englishRussianDict.ContainsKey(word))
+                Console.WriteLine(englishRussianDict[word]);
+            else
+                Console.WriteLine(word + "doesn't exist");
+            Console.ReadKey();
+        }
+
+        void FindR()
+        {
+            Console.Clear();
+
+            string word;
+            Console.Write("Enter russian word: ");
+            word = Console.ReadLine().ToLower();
+            if (englishRussianDict.ContainsValue(word))
+            {
+                foreach (var item in englishRussianDict)
+                {
+                    if (item.Value == word)
+                    {
+                        Console.WriteLine(item);
+                        break;
+                    }
+                }
+            }
+            else
+                Console.WriteLine(word + "doesn't exist");
+            Console.ReadKey();
+
+        }
+
+        void ShowDict()
+        {
+            Console.Clear();
+            foreach (var item in englishRussianDict)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+        public void Start()
+        {
+            char choise;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1.Add word");
+                Console.WriteLine("2.Delete word");
+                Console.WriteLine("3.Enter english get russian");
+                Console.WriteLine("4.Enter russian get english");
+                Console.WriteLine("5.Show dictionary");
+                Console.WriteLine("6.Exit");
+                choise = Convert.ToChar(Console.ReadLine());
+                switch (choise)
+                {
+                    case '1':
+                        AddWord();
+                        break;
+                    case '2':
+                        DeleteWord();
+                        break;
+                    case '3':
+                        FindE();
+
+                        break;
+                    case '4':
+                        FindR();
+
+                        break;
+                    case '5':
+                        ShowDict();
+                        break;
+                    case '6':
+                        Console.Clear();
+                        return;
+                }
+
+            }
+        }
+    }
+}
---
+++ lab_28.02.2023/lab_28.02.2023/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            EnglishRussianDict dict= new EnglishRussianDict();
+            dict.Start();
+        }
+
+        static void Task2()
+        {
+            string[] words;
+            Dictionary<string, int>  dict = new Dictionary<string, int>();
+            Console.WriteLine("Enter text: ");
+            words = Console.ReadLine().ToLower().Split(' ');
+            foreach (string word in words) 
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                    continue;
+                }
+                dict.Add(word, 1);
+            }
+            foreach (var el in dict) 
+            {
+                Console.WriteLine(el.Key + "\t" + el.Value);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+       Task1();
+            Task2();
+        }
+    }
+}
