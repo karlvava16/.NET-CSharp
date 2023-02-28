32e10c9d15533af77d1ede62dc498e80491e9a37
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-02-28 12:19:14 +0200

    hw_27.02.2023

--- hw_27.02.2023/hw_27.02.2023/Program.cs
+++ hw_27.02.2023/hw_27.02.2023/Program.cs
@@ -8,8 +8,25 @@ namespace Homework
 {
     internal class Program
     {
+        static void MySwap<T>(ref T value1, ref T value2)
+        {
+            T temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
         static void Main(string[] args)
         {
+            //Task1
+            int value1 = 12, value2 = 5;
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+
+            MySwap(ref value1, ref value2);
+
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+
+
         }
     }
 }
