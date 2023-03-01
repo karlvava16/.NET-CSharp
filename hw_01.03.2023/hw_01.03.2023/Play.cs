b0344c86489f59bca6c6b4c1e52b0832831ef787
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-03-01 20:45:22 +0200

    hw_01.03.2023

---
+++ hw_01.03.2023/hw_01.03.2023/Play.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Play : IDisposable
+    {
+        public string PlayName { get; set; }
+        public string Gendre { get; set; }
+        public string Author { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        private bool disposed = false;
+
+        public Play () { }
+
+        public Play(string playName, string gendre, string author, DateTime createDate)
+        {
+            PlayName = playName;
+            Gendre = gendre;
+            Author = author;
+            CreateDate = createDate;
+        }
+
+
+        protected virtual void Clean(bool disposing)
+        {
+
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+
+                }
+
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Clean(true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("Object disposed Play");
+        }
+        ~Play()
+        {
+            Clean(false);
+            Console.WriteLine("Object destoy Play");
+        }
+
+        public override string ToString()
+        {
+            return $"Play : \"{PlayName}\"\nGendre : {Gendre}\nAuthor : {Author}\n Date : {CreateDate.ToString("dd.MM.yyyy")}\n";
+        }
+    }
+}
---
+++ hw_01.03.2023/hw_01.03.2023/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Homework
+{
+    enum Types
+    {
+        Producted,
+        House,
+        Clothes,
+        Shoes
+    };
+    class Shop : IDisposable
+    {
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public int Type { get; set; }
+
+        public Shop()
+        {
+            Name = null;
+            Address = null;
+            Type = -1;
+
+        }
+        public Shop(string Name, string Address, int type)
+        {
+            this.Name = Name;
+            this.Address = Address;
+            Type = type;
+        }
+        ~Shop()
+        {
+            Console.WriteLine("Object destoy Shop");
+        }
+        private bool disposed = false;
+        protected virtual void Clean(bool disposing)
+        {
+
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+
+                }
+
+            }
+            disposed = true;
+        }
+        public void Dispose()
+        {
+            Clean(true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("Object disposed Shop");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            for (int i = 0; i < 3; i++)
+            {
+                Play a = new Play("Name", "Surname", "Author_name", new DateTime(2023,03,01));
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                using (Play z = new Play())
+                {
+
+                };
+            }
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            //task2
+            for (int i = 0; i < 3; i++)
+            {
+                using (Shop s = new Shop())
+                {
+
+                };
+
+            }
+        }
+    }
+}
