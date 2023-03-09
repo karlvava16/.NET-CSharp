44bb3358060a50135634d4b6f48cd2e9c85c5f30
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-03-09 10:45:39 +0200

    hm_03.03.2023

--- hw_03.03.2023/hw_03.03.2023/Program.cs
+++ hw_03.03.2023/hw_03.03.2023/Program.cs
@@ -2,15 +2,112 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Homework
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //Task1();
+            //Task2();
+            //Task3();
+            //Task4();
+            //Task5();
+            //Task6();
+            //Task7();
+            Task8();
+        }
+
+        static void Task1()
+        {
+            Regex regex = new Regex("a\\Sb");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
 
         }
+
+        static void Task2() 
+        {
+            Regex regex = new Regex("a\\S{2}a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static void Task3()
+        {
+            Regex regex = new Regex("a\\S[^e]{1}a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static void Task4()
+        {
+            Regex regex = new Regex("ab{1,}a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static void Task5()
+        {
+            Regex regex = new Regex("ab{0,}a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static void Task6()
+        {
+            Regex regex = new Regex("ab?a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+        static void Task7()
+        {
+            Regex regex = new Regex("ab?a");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        static void Task8()
+        {
+            Regex regex = new Regex("(ab){1,}");
+            Console.WriteLine("Enter string:");
+            string str = Console.ReadLine();
+            foreach (var r in regex.Matches(str))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
     }
 }
