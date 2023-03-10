1c797625f2b3554da695ea24bc128cee1c4f03c6
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2023-03-10 11:11:44 +0200

    hw_10.03.2023 and hw_08.03.2023

---
+++ hw_08.03.2023/hw_08.03.2023/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+
+    public class Device
+    {
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Cost { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание массивов устройств
+            Device[] array1 = {
+            new Device { Name = "Телефон", Manufacturer = "Apple", Cost = 999 },
+            new Device { Name = "Ноутбук", Manufacturer = "Dell", Cost = 1500 },
+            new Device { Name = "Планшет", Manufacturer = "Samsung", Cost = 600 }
+        };
+
+            Device[] array2 = {
+            new Device { Name = "Телефон", Manufacturer = "Samsung", Cost = 799 },
+            new Device { Name = "Телевизор", Manufacturer = "LG", Cost = 1200 },
+            new Device { Name = "Ноутбук", Manufacturer = "Dell", Cost = 1200 }
+        };
+
+            // Разница массивов
+            var diffArray = array1.Except(array2, new DeviceComparer());
+
+            Console.WriteLine("Разница массивов:");
+            foreach (var device in diffArray)
+            {
+                Console.WriteLine($"{device.Name} ({device.Manufacturer}): {device.Cost}$");
+            }
+
+            // Пересечение массивов
+            var intersectArray = array1.Intersect(array2, new DeviceComparer());
+
+            Console.WriteLine("\nПересечение массивов:");
+            foreach (var device in intersectArray)
+            {
+                Console.WriteLine($"{device.Name} ({device.Manufacturer}): {device.Cost}$");
+            }
+
+            // Объединение массивов
+            var unionArray = array1.Union(array2, new DeviceComparer());
+
+            Console.WriteLine("\nОбъединение массивов:");
+            foreach (var device in unionArray)
+            {
+                Console.WriteLine($"{device.Name} ({device.Manufacturer}): {device.Cost}$");
+            }
+        }
+
+        // Класс, реализующий интерфейс IEqualityComparer для сравнения устройств по производителю
+        class DeviceComparer : IEqualityComparer<Device>
+        {
+            public bool Equals(Device x, Device y)
+            {
+                return x.Manufacturer == y.Manufacturer;
+            }
+
+            public int GetHashCode(Device device)
+            {
+                return device.Manufacturer.GetHashCode();
+            }
+        }
+    }
+
+}
---
+++ hw_10.03.2023/hw_10.03.2023/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Data;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Homework
+{
+    [Serializable]
+    [DataContract]
+    public class Device
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Manufacturer { get; set; }
+
+        [DataMember]
+        public decimal Cost { get; set; }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream stream = null;
+            XmlSerializer serializer = null;
+            DataContractJsonSerializer jsonFormatter = null;
+            Device d=new Device();
+
+            d = new Device { Name = "Телефон", Manufacturer = "Apple", Cost = 999 };
+            
+            stream = new FileStream("../../data.xml", FileMode.Create);
+            serializer = new XmlSerializer(typeof(Device));
+            serializer.Serialize(stream, d);
+            stream.Close();
+            Console.WriteLine("Сериализация успешно выполнена!");
+
+            stream = new FileStream("../../data.json", FileMode.Create);
+            jsonFormatter = new DataContractJsonSerializer(typeof(Device));
+            jsonFormatter.WriteObject(stream, d);
+            stream.Close();
+            Console.WriteLine("Сериализация успешно выполнена!");
+        }
+    }
+}
